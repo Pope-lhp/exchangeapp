58462f84a3b10520edb4311a84b822c1be606164
Pope-lhp <2694932690@qq.com>
2026-03-20 12:38:20 +0800

    实现了密码加密的功能，使用Bcrypt算法进行加密

--- controllers/auth_controllers.go
+++ controllers/auth_controllers.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"exchangeapp/models"
+	"exchangeapp/utils"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -10,7 +11,13 @@ import (
 func Register(ctx *gin.Context) {
 	var user models.User
 	if err := ctx.ShouldBind(&user); err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()}) //绑定参数
 		return
 	}
+	hashedPwd, error := utils.HashPassword(user.Password)
+	if error != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": error.Error()})
+		return
+	}
+	user.Password = hashedPwd
 }
