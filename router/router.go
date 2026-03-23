49d0ac0840dd3769d86bb356242a77caaf47d5b0
Pope-lhp <2694932690@qq.com>
2026-03-23 21:52:14 +0800

    将路由中匿名函数改为对应的函数路径，成功实现项目的注册和登录部分！（今天十分高兴终于把这个demo的很重要的一部分跑通了，继续加油）

--- router/router.go
+++ router/router.go
@@ -3,30 +3,19 @@
 package router
 
 import (
-	"net/http"
+	"exchangeapp/controllers"
 
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
-	/*创建路由分组：把所有 /api/auth 开头的接口归为一组
-	r.Group("/api/auth")：创建分组，前缀是 /api/auth
-	auth：分组名，后续所有以 auth. 开头的接口，都会自动带上 /api/auth 前缀
-	作用：简化路径写法，比如 auth.POST("/login") 等价于 r.POST("/api/auth/login")，代码更整洁*/
+
 	auth := r.Group("/api/auth")
 	{
-		auth.POST("/login", func(ctx *gin.Context) {
-			ctx.AbortWithStatusJSON(http.StatusOK, gin.H{
-				"msg": "Login Success",
-			})
-		})
+		auth.POST("/login", controllers.Login)
 
-		auth.POST("/register", func(ctx *gin.Context) {
-			ctx.AbortWithStatusJSON(http.StatusOK, gin.H{
-				"msg": "Register Success",
-			})
-		})
+		auth.POST("/register", controllers.Register)
 	}
 	return r
 }
