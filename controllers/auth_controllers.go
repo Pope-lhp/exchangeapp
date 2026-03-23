09f601794939cd7e864606f988901418d868e0a7
Pope-lhp <2694932690@qq.com>
2026-03-23 19:41:17 +0800

    加入了登录功能

--- controllers/auth_controllers.go
+++ controllers/auth_controllers.go
@@ -38,3 +38,29 @@ func Register(ctx *gin.Context) {
 
 	ctx.JSON(http.StatusOK, gin.H{"token": token}) //将生成的jwt令牌返回给前端
 }
+
+func Login(ctx *gin.Context) {
+	var input struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+	if err := ctx.ShouldBind(&input); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	var user models.User
+	if err := global.Db.Where("username = ?", input.Username).Error; err != nil {
+		ctx.JSON(http.StatusUnauthorized, gin.H{"error": "wrong credentials"})
+		return
+	}
+	if !utils.CheckPassword(input.Password, user.Password) {
+		ctx.JSON(http.StatusUnauthorized, gin.H{"error": "wrong credentials"})
+		return
+	}
+	token, err := utils.GenerateJWT(user.Username)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"token": token})
+}
