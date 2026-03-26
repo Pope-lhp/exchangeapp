05bc95fb59bcb077ff9202974f1f43714e7cf4a2
Pope-lhp <2694932690@qq.com>
2026-03-26 09:00:51 +0800

    中间件，验证jwt是否有效

--- router/router.go
+++ router/router.go
@@ -4,18 +4,24 @@ package router
 
 import (
 	"exchangeapp/controllers"
+	"exchangeapp/middlewares"
 
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
-
 	auth := r.Group("/api/auth")
 	{
 		auth.POST("/login", controllers.Login)
-
 		auth.POST("/register", controllers.Register)
+	}
+	api := r.Group("/api")
+	api.GET("/exchangeRates", controllers.GetExchangeRate)
+	api.Use(middlewares.AuthMiddleWare()) //给api路由组下所有接口统一添加身份校验
+	{
+		api.POST("/exchangeRates", controllers.CreateExchangeRate)
+
 	}
 	return r
 }
