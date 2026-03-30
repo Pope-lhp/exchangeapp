6900761e6c14c8db6251418d7b62e54202d17d29
Pope-lhp <2694932690@qq.com>
2026-03-30 19:28:35 +0800

    利用redis，实现点赞功能

---
+++ controllers/like_controller.go
@@ -0,0 +1,39 @@
+package controllers
+
+import (
+	"exchangeapp/global"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis"
+)
+
+func LikeArticle(ctx *gin.Context) {
+	articleID := ctx.Param("id") //获取动态参数id
+
+	likeKey := "article:" + articleID + ":likes" /*Redis 键名（Key）设计规范
+	[实体名] : [唯一ID] : [属性名]*/
+	if err := global.RedisDB.Incr(likeKey).Err(); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"message": "Successfully liked article"})
+
+}
+
+func GetArticleLikes(ctx *gin.Context) {
+	articleID := ctx.Param("id")
+
+	likeKey := "article:" + articleID + ":likes"
+
+	likes, err := global.RedisDB.Get(likeKey).Result()
+
+	if err == redis.Nil {
+		likes = "0"
+	} else if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"likes": likes})
+
+}
